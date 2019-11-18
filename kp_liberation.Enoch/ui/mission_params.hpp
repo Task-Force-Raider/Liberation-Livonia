6c8a7c492ac192ba2fbea4fc66d8c82baf0a2f26
McFur <calcitrim@hotmail.com>
2019-11-18 23:45:48 +1300

    Update mission_params.hpp
    arsenal restrictions test
    victory condition set to 4

--- kp_liberation.Enoch/ui/mission_params.hpp
+++ kp_liberation.Enoch/ui/mission_params.hpp
@@ -124,7 +124,7 @@ class Params
         title = $STR_PARAMS_VICTORYCONDITION;
         values[] = {0, 1, 2, 3, 4};
         texts[] = {$STR_PARAMS_VICTORYCONDITION_0, $STR_PARAMS_VICTORYCONDITION_1, $STR_PARAMS_VICTORYCONDITION_2, $STR_PARAMS_VICTORYCONDITION_3, $STR_PARAMS_VICTORYCONDITION_4};
-        default = 0;
+        default = 4;
     };
     class Spacer1 {
         title = "";
@@ -208,7 +208,7 @@ class Params
         title = $STR_PARAMS_ARSENALUSEPRESET;
         values[] = { 0, 1 };
         texts[] = { $STR_PARAMS_NORESTRICTIONS, $STR_PARAMS_USEPRESET };
-        default = 0;
+        default = 1;
     };
     class MapMarkers {
         title = $STR_PARAMS_MAPMARKERS;
